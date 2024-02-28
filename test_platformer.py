ae961e9bd61341ec7b70718cd0eaf8ff0262eea1
nathanblanchard <njblanch@uvm.edu>
2024-02-28 16:30:44 -0500

    Added some modified example code from Python Arcade + tiled map

---
+++ test_platformer.py
@@ -0,0 +1,171 @@
+"""
+Platformer Template
+"""
+import arcade
+
+# --- Constants
+SCREEN_TITLE = "Platformer"
+
+SCREEN_WIDTH = 256
+SCREEN_HEIGHT = 240
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 1
+SPRITE_PIXEL_SIZE = 16
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 3
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 15
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self):
+
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT,
+                         SCREEN_TITLE, resizable=True)
+
+        # Our TileMap Object
+        self.tile_map = None
+
+        # Our Scene Object
+        self.scene = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+
+        # What key is pressed down?
+        self.left_key_down = False
+        self.right_key_down = False
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        src = "resources/demo_sprite.png"
+        self.player_sprite = arcade.Sprite(src, CHARACTER_SCALING)
+        self.player_sprite.center_x = 16
+        self.player_sprite.center_y = 16
+        self.player_list.append(self.player_sprite)
+
+        # Name of map file to load
+        map_name = "resources/demo_background.json"
+
+        # Layer specific options are defined based on Layer names in a dictionary
+        # Doing this will make the SpriteList for the platforms layer
+        # use spatial hashing for detection.
+        layer_options = {
+            "Platforms": {
+                "use_spatial_hash": True,
+            },
+            "Background": {
+                "use_spatial_hash": True,
+            },
+        }
+
+        # Read in the tiled map
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+
+        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
+
+        # Initialize Scene with our TileMap, this will automatically add all layers
+        # from the map as SpriteLists in the scene in the proper order.
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        # Set wall List
+        self.wall_list = self.tile_map.sprite_lists["Platforms"]
+
+        # Set background image
+        self.background_list = self.tile_map.sprite_lists["Background"]
+
+        # --- Other stuff
+        # Create the 'physics engine'
+        walls = [self.wall_list, ]
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=walls
+        )
+
+    def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        self.clear()
+
+        # Draw our Scene
+        # Note, if you a want pixelated look, add pixelated=True to the parameters
+
+        self.background_list.draw()
+        self.player_list.draw()
+        self.wall_list.draw()
+        
+
+        #self.scene.draw()
+
+
+    def update_player_speed(self):
+
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
+
+        if self.left_key_down and not self.right_key_down:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif self.right_key_down and not self.left_key_down:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        # Jump
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+
+        # Left
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_key_down = True
+            self.update_player_speed()
+
+        # Right
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_key_down = True
+            self.update_player_speed()
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_key_down = False
+            self.update_player_speed()
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_key_down = False
+            self.update_player_speed()
+
+    def on_update(self, delta_time):
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+
+def main():
+    """Main function"""
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
