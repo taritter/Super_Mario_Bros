2c8b4a158949c2e8b5b943e3302c30d71f0a1a02
rachel-rose <rachel.rose@uvm.edu>
2024-04-11 14:32:46 -0400

    Revert "deleted goomba layer. Enemies layer has constant movement"
    This reverts commit 4c181f4f70528c9568381d31fc74b88f6fa15ad5.

--- resources/backgrounds/goomba_move.tsx
+++ resources/backgrounds/goomba_move.tsx
@@ -2,13 +2,13 @@
 <tileset version="1.10" tiledversion="1.10.2" name="goomba_move" tilewidth="16" tileheight="16" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="3">
-  <image width="16" height="16" source="../../../../../CS3050-Final-Project/resources/sprites/goomba_1.png"/>
+  <image width="16" height="16" source="../../../../VSCode Projs/CS3050-Final-Project/resources/sprites/goomba_1.png"/>
   <animation>
    <frame tileid="3" duration="200"/>
    <frame tileid="4" duration="200"/>
   </animation>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="../../../../../CS3050-Final-Project/resources/sprites/goomba_2.png"/>
+  <image width="16" height="16" source="../../../../VSCode Projs/CS3050-Final-Project/resources/sprites/goomba_2.png"/>
  </tile>
 </tileset>
