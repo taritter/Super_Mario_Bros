6f2ad6ecc553dae3fdc0d48c63ad833a8ca1c8ba
nathanblanchard <njblanch@uvm.edu>
2024-04-15 00:47:49 -0400

    Removed some old and unused files

--- resources/backgrounds/1-1/tileset_1-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset_1-1" tilewidth="16" tileheight="16" tilecount="6330" columns="211">
- <image source="world_1-1.png" width="3376" height="480"/>
-</tileset>
